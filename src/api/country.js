4a1d18ef75878a34680a63f064cdc623a36aed4a
Emmanuel Julius Samson <juliemmasam@gmail.com>
2022-04-19 19:11:48 +0300

    Refactor country specific APIs

---
+++ src/api/country.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const functions = require('../models/location/functions');
+
+const countryApis = express.Router();
+
+countryApis.route('/')
+.get(async (req, res)=>{
+  functions.getAllCountries()
+  .then(results => res.send(results))
+  .catch(err => res.send(err.errors));
+})
+.post(async (req, res)=>{
+  functions.addCountry(req.body)
+  .then(response => res.send(response))
+  .catch(err => res.send(err.errors));
+})
+.delete(async (req, res) => {
+  functions.deleteCountry(req.body)
+  .then(response => res.send(response))
+  .catch(err => res.send(err.errors));
+})
+.put(async (req, res) => {
+  functions.updateCountry(req.body)
+  .then(response => res.send(response))
+  .catch(err => res.send(err.errors));
+});
+
+module.exports = { countryApis };
--- src/server.js
+++ src/server.js
@@ -2,33 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const functions = require('./models/location/functions');
 
+const { countryApis } = require('./api/country');
+
 const server = express();
 const port = 3000;
 
 server.use(bodyParser.json());
-server.get('/', async (req, res)=>{
-  functions.getAllCountries()
-  .then(results => res.send(results))
-  .catch(err => res.send(err.errors));
-});
-
-server.post('/', async (req, res)=>{
-  functions.addCountry(req.body)
-  .then(response => res.send(response))
-  .catch(err => res.send(err.errors));
-});
-
-server.delete('/', async (req, res) => {
-  functions.deleteCountry(req.body)
-  .then(response => res.send(response))
-  .catch(err => res.send(err.errors));
-});
-
-server.put('/', async (req, res) => {
-  functions.updateCountry(req.body)
-  .then(response => res.send(response))
-  .catch(err => res.send(err.errors));
-});
+server.use('/country', countryApis);
 
 server.listen(port, ()=>{
   console.log("The server is actively listening");
