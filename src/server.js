06ed19714a583b0cbd260f2d2008edf572f8ff1a
Emmanuel Julius Samson <juliemmasam@gmail.com>
2022-04-20 14:46:49 +0300

    Expose region APIs

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { countryApis } = require('./api/country');
+const { regionApis } = require('./api/regions');
 
 const server = express();
 const port = 3000;
 
 server.use(bodyParser.json());
 server.use('/country', countryApis);
+server.use('/regions', regionApis);
 
 server.listen(port, ()=>{
   console.log("The server is actively listening");
